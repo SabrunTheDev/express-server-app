a5565f02e1525c2d07d2d9c01e78bb466e451625
Sabrun <officialsabrun@gmail.com>
2024-03-17 06:26:26 -0400

    Updated files

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const port = 3000;
 const bodyParser = require("body-parser");
 const fs = require("fs");
 
+app.use(express.static("css"));
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  // res.send("Hello");
+  res.render("index", { text: "World" });
 });
 
 app.listen(port, () => {
