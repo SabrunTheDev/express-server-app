bb8ef208afef9e7cee197264ea1e36947d44740b
Sabrun <officialsabrun@gmail.com>
2024-03-17 00:10:35 -0400

    Updated files

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 app.listen(port, () => {
   console.log(`This server is running on http://localhost:${port}`);
