2e10ce37d4b531244350622caae40f2261414d83
Sabrun <officialsabrun@gmail.com>
2024-03-17 23:38:34 -0400

    Updated index.html

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000;
 const bodyParser = require("body-parser");
 const fs = require("fs");
 
+const players = require("./data/players");
+const teams = require("./data/teams");
+
 app.use(express.static("css"));
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,6 +15,10 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   // res.send("Hello");
   res.render("index", { text: "World" });
+});
+
+app.get("/api/players", (req, res) => {
+  res.json(players);
 });
 
 app.listen(port, () => {
